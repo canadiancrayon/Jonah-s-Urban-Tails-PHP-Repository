ea0144c45b5388bb46a572a008e0345aeec1d152
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:39:47 -0300

    Add location.php for contact information page

---
+++ pages/location.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?php include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey">
+      <h3>Contact Us</h3>
+      <p>jonahsurbantails@gmail.com</p>
+      <p>(902) 441-0813</p>
+      <p>Or leave a message on our Facebook page: Jonah's Urban Tails</p>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
