f4f8512f30e29d542746de0a0a453a080b369507
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:39:28 -0300

    Add sitemap.php for site navigation

---
+++ pages/sitemap.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?php include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey" style="padding-left:0; padding-right:0;">
+      <div class="w3-half">
+        <ul class="w3-ul">
+          <li class="w3-gray"><h4><a href="../my_business.php">Home</a></h4></li>
+        </ul>
+        <ul class="w3-ul w3-border-top">
+          <li class="w3-gray"><h4>Online Booking</h4></li>
+          <li><a href="product_catalog.php">Product Catalog</a></li>
+          <li><a href="shopping_cart.php">Shopping Cart</a></li>
+          <li><a href="checkout.php">Checkout</a></li>
+          <li><a href="register.php">Register</a></li>
+          <li><a href="login.php">Login</a></li>
+          <li><a href="../scripts/logout.php">Logout</a></li>
+        </ul>
+      </div>
+      <div class="w3-half w3-border-left">
+        <ul class="w3-ul">
+          <li class="w3-gray"><h4>Events</h4></li>
+          <li><a href="events_recurring.php">Recurring</a></li>
+          <li><a href="events_oneoff.php">Upcoming One-Offs</a></li>
+          <li><a href="events_archived.php">Archived</a></li>
+        </ul>
+        <ul class="w3-ul w3-border-top">
+          <li class="w3-gray"><h4>About Us</h4></li>
+          <li><a href="vision.php">Vision and Mission</a></li>
+          <li><a href="locations.php">Contact Us</a></li>
+          <li><a href="feedback.php">Feedback Form</a></li>
+        </ul>
+        <ul class="w3-ul w3-border-top">
+          <li class="w3-gray"><h4><a href="sitemap.php">Site Map</a></h4></li>
+        </ul>
+      </div>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
