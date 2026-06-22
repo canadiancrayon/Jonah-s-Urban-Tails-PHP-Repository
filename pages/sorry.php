c83f31e8d319903ed26dedfae1c7885bc8a2d753
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:41:28 -0300

    Add sorry.php for error handling page

---
+++ pages/sorry.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey">
+      <h1 class="w3-margin w3-center">Sorry :(</h1>
+      <h2 class="w3-margin w3-center">This page isn't working right now, try again later.</h2>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
