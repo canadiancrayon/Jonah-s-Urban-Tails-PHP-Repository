1153059ed11242d12704b55359b04807698f7ff8
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:41:10 -0300

    Add recurring events page

---
+++ pages/events_recurring.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?php include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey">
+      <h3>Recurring Events</h3>
+      <ul>
+        <li>Best way to take care of your pet</li>
+        <li>How to train your pet effectively</li>
+        <li>The best foods for your pet</li>
+      </ul>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
