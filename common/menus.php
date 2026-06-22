cf60c8c8e4df0508d221b10173ad9d626e5d8c51
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:15:18 -0300

    Add navigation menu to menus.php

---
+++ common/menus.php
@@ -0,0 +1,35 @@
+<nav class="w3-container">
+    <div class="w3-bar w3-blue" style="display:flex; justify-content:center">
+        <a href="my_business.php" class="w3-bar-item w3-button"><i class="material-icons">home</i></a>
+        <div class="w3-dropdown-hover">
+            <button class="w3-button">Online Booking &#x25BC;</button>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a href="pages/product_catalog.php" class="w3-bar-item w3-button">Product Catalog</a>
+                <a href="pages/estore.php" class="w3-bar-item w3-button">Booking options</a>
+                <a href="pages/shopping_cart.php" class="w3-bar-item w3-button">Shopping Cart</a>
+                <a href="pages/checkout.php" class="w3-bar-item w3-button">Checkout</a>
+                <a title="Not yet active" href="pages/sorry.php" class="w3-bar-item w3-button">Overnight stay</a>
+                <a href="pages/register.php" class="w3-bar-item w3-button">Register</a>
+                <a href="pages/login.php" class="w3-bar-item w3-button">Login</a>
+                <a href="scripts/logout.php" class="w3-bar-item w3-button">Logout</a>
+            </div>
+        </div>
+        <div class="w3-dropdown-hover">
+            <button class="w3-button">Events &#x25BC;</button>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a href="pages/events_recurring.php" class="w3-bar-item w3-button">Recurring</a>
+                <a href="pages/events_oneoff.php" class="w3-bar-item w3-button">Upcoming One-Offs</a>
+                <a href="pages/events_archived.php" class="w3-bar-item w3-button">Archived</a>
+            </div>
+        </div>
+        <div class="w3-dropdown-hover">
+            <button class="w3-button">About Us &#x25BC;</button>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a href="pages/vision.php" class="w3-bar-item w3-button">Vision and Mission</a>
+                <a href="pages/locations.php" class="w3-bar-item w3-button">Contact Us</a>
+                <a href="pages/feedback.php" class="w3-bar-item w3-button">Feedback Form</a>
+            </div>
+        </div>
+        <a href="pages/sitemap.php" class="w3-bar-item w3-button">Site Map</a>
+    </div>
+</nav>
