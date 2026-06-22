57096ce0d293830e707757ab14ae8e3ff380149b
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:40:29 -0300

    Add events_archived.php for displaying archived events

---
+++ pages/events_archived.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?php include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey">
+      <h3>Archived Events</h3>
+      <dl>
+        <dt>December 19, 2025</dt>
+        <dd>Puppy training at an indoor gym</dd>
+        <dt>January 13, 2026</dt>
+        <dd>Pet adoption event at a local shelter</dd>
+      </dl>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
