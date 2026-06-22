2371d79c8877f5fa24346fa969426c66a7c1df7d
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:13:50 -0300

    Add head.php with HTML head elements

---
+++ common/head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="/~u07/submissions/submission06/">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
+    <link rel="stylesheet" href="css/my_css.css">
+    <link rel="icon" type="image/jpg" href="images/smiley_face.jpg">
+    <title>Jonah's Urban Tails</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="scripts/slideshow.js" defer></script>
+</head>
