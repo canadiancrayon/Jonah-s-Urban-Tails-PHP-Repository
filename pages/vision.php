4b6d71cb2297dc3ad3a8ed34018964b1d76e758e
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:40:03 -0300

    Create vision.php

---
+++ pages/vision.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?php include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey">
+      <h3>Jonah's Urban Tails - Our Vision and Mission</h3>
+      <p>Starting back in 2018, Jonah's Urban Tails was created with the vision of high-quality pet-sitting that doesn't break the bank. We believe that pets are more comfortable in their own homes...</p>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
