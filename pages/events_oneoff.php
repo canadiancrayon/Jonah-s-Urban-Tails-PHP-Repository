8d698b0faf2134e65969f74f1e9a4ab73506000f
Jonah B. <jonahbucci@gmail.com>
2026-06-22 15:40:50 -0300

    Create events_oneoff.php for upcoming events
    Added a new page for upcoming one-off events with details.

---
+++ pages/events_oneoff.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../common/head.php'; ?>
+<body class="my_max_site_width w3-auto">
+  <?php include '../common/banner.php'; ?>
+  <?php include '../common/menus.php'; ?>
+  <main class="w3-container">
+    <div class="w3-container w3-border-left w3-border-right w3-border-black w3-light-grey">
+      <h3>Upcoming One-Off Events</h3>
+      <ul>
+        <li>Pet first aid workshop - July 15, 2026</li>
+        <li>How to keep good habits for your pet - August 1st, 2026</li>
+        <li>Puppy yoga - August 15th, 2026</li>
+      </ul>
+    </div>
+  </main>
+  <?php include '../common/footer.php'; ?>
+</body>
+</html>
